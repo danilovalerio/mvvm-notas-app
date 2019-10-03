f77f66e1253d4edff5aef6cf827ddca33b667b63
Danilo Valerio Silva <dvalerio@everis.com>
2019-10-03 14:04:28 -0300

    Responsabilidade de add nota e obter notas passada para DATABASE

---
+++ app/src/main/java/com/example/notasmvvm/data/Database.kt
@@ -0,0 +1,22 @@
+package com.example.notasmvvm.data
+
+import androidx.lifecycle.MutableLiveData
+
+class Database {
+
+    private val mDatabase: MutableLiveData<MutableList<Nota>> = MutableLiveData()
+
+    fun inserirNota(nota: Nota){
+        var tmp = mDatabase.value
+        if(tmp == null){
+            tmp = mutableListOf()
+            tmp.add(nota)
+        } else {
+            tmp?.add(nota)
+        }
+        mDatabase.postValue(tmp)
+    }
+
+    //Retorna somente o valor da lista de dados
+    fun obterNotas() = mDatabase.value
+}
--- app/src/main/java/com/example/notasmvvm/data/GestorDeNotas.kt
+++ app/src/main/java/com/example/notasmvvm/data/GestorDeNotas.kt
@@ -1,37 +1,19 @@
 package com.example.notasmvvm.data
 
-import android.util.Log
-import androidx.lifecycle.MutableLiveData
+/**
+ * Pega as notas do banco, do firebase etc...
+ * Possui a responsabilidade somente de gerenciar os dados
+ */
 
-//Pega as notas do banco, do firebase etc...
 class GestorDeNotas {
 
-    private val data: MutableLiveData<MutableList<Nota>> = MutableLiveData()
+    //Dependência para gestorDeNotas
+    private val database = Database()
 
-//    private val data = mutableListOf<Nota>(
-//        Nota(1,"Nota 1"),
-//        Nota(2,"Nota 2"),
-//        Nota(3,"Nota 3"),
-//        Nota(4,"Nota 4"),
-//        Nota(5,"Nota 5"),
-//        Nota(6,"Nota 6"),
-//        Nota(7,"Nota 7"),
-//        Nota(8,"Nota 8"),
-//        Nota(9,"Nota 9"),
-//        Nota(10,"Nota10")
-//    )
-
-    fun getNotas() = data
+    fun getNotas() = database.obterNotas()
 
     fun addNota(mNota: Nota){
-        var tmp = data.value
-        if(tmp == null){
-            tmp = mutableListOf()
-            tmp.add(mNota)
-        } else {
-            tmp?.add(mNota)
-        }
-        data.postValue(tmp)
+        database.inserirNota(mNota)
     }
 
 }
